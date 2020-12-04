d5ab8fc9c7c7c7e7f69637fb9f1d7e4ec0443648
Domen Oslaj <doslaj@gmail.com>
2020-12-04 21:57:09 +0100

    jinja included

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ def index():
 
 @app.route("/about-me")
 def about():
-    return render_template("about.html")
+    name = "Domen Oslaj"
+    age = 30
+    address = "Filovci 45, 9222 Bogojina, Slovenia"
+
+    best_friends = ["Miha, Luka, Grega, Lolek, Bolek"]
+
+    return render_template("about.html", name=name, age=age, address=address, best_friends=best_friends)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5001)
+
